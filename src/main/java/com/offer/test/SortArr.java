f33f282b74f8ab085ffef23f217709ba4e932f15
zhouwenchen <761075116@qq.com>
2020-08-07 16:46:57 +0800

    提交一些测试案例

--- src/main/java/com/offer/test/SortArr.java
+++ src/main/java/com/offer/test/SortArr.java
@@ -1,6 +1,10 @@
 package com.offer.test;
 
-/**  
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
  * 
  * 样例  1:
 	输入: A=[1], B=[1]
@@ -72,7 +76,45 @@ public class SortArr {
 		}
 		System.out.println(C.toString());
 	}
-	
+
+	/**
+	 * 将两个有序的数组添加到更大的数组中，然后直接使用Collections.sort()
+	 * @param A
+	 * @param B
+	 */
+	public static void sortArr1(int[] A, int[] B) {
+		int[] C = new int[A.length + B.length];
+		System.arraycopy(A,0,C,0,A.length);
+		System.arraycopy(B,0,C,A.length,B.length);
+		Arrays.sort(C);
+	}
+
+	/**
+	 * 创建一个大数组，从数组末尾开始填充数据,
+	 * @param A
+	 * @param B
+	 */
+	public static void sortArr2(int[] A, int[] B) {
+		int alen = A.length - 1;
+		int blen = B.length - 1;
+		int[] C = new int[A.length + B.length];
+		int clen = C.length - 1;
+		while (alen >= 0 && blen >= 0) {
+			if (A[alen] > B[blen]) {
+				C[clen--] = A[alen--];
+			} else {
+				C[clen--] = B[blen--];
+			}
+		}
+		// 判断 A 或者 B已经没有数据了
+		if (alen >= 0) {
+			System.arraycopy(A, 0, C, 0, alen - blen);
+		}
+		if (blen >= 0) {
+			System.arraycopy(B, 0, C, 0, blen - alen);
+		}
+		System.out.println(C);
+	}
 	
 	/**
 	 * 问题1，可能某一个数组的最大值小于两个数组的最小值。
@@ -88,8 +130,11 @@ public class SortArr {
 //		int[] B = new int[] { 5, 6, 7, 8 };
 		
 		// 测试案例3
-		int[] A = new int[] { };
-		int[] B = new int[] { 5, 6, 7, 8 };
-		sortArr(A, B);
+//		int[] A = new int[] { };
+//		int[] B = new int[] { 5, 6, 7, 8 };
+
+ 		int[] A = new int[] { 1,3,5};
+		int[] B = new int[] { 2, 4, 6, 8 };
+		sortArr2(A, B);
 	}
 }
